34bca3df65ca19144648610874e0c4328e905ecd
Wilmer Morales <moralesw.1981@gmail.com>
2024-08-19 15:02:12 -0500

    service event

--- src/services.js
+++ src/services.js
@@ -2,12 +2,46 @@ import { getServices } from "./database.js"
 
 const services = getServices()
 
+
+// const findCelebrityMatch = (kidObject, celebrityArray) => {
+//     let celebrityMatch = null
+
+//     for (const celebrity of celebrityArray) {
+//       if(celebrity.id === kidObject.celebrityId ){
+//         celebrityMatch = celebrity
+//       }
+//     }
+//     return celebrityMatch
+// }
+
+// const findAreaMatch = (service, allAreas) => {
+//     let findArea = null
+//     for (const area of allAreas) {
+//         if(area.id === service.id) {
+//             findArea = area
+//         }
+        
+//     }
+//     return findArea
+// }
+
+document.addEventListener(
+    "click",
+    (clickEvt) => {
+        const clickTarget = clickEvt.target
+        if(clickTarget.dataset.type === "services") {
+            alert(`${clickTarget.dataset.name}  is available in  ${clickTarget.dataset.serviceareas}`)
+        }
+    }
+)
+
 export const servicesList = () => {
     
     let servicesListHTML = "<ul>";
 
     for (const service of services) {
-        servicesListHTML += `<li data-type ="services" data-id="${service.id}">${service.service}</li>`
+        const formattedAreas = service.serviceAreas.split(',').map(area => area.trim()).join(', ');
+        servicesListHTML += `<li data-type ="services" data-serviceareas="${service.serviceAreas}" data-name="${service.service}" data-id="${service.id}">${service.service}</li>`
         // Convert each service object to an <li> and append to the serviceListHTML string
     }
 
@@ -15,3 +49,7 @@ export const servicesList = () => {
 
     return servicesListHTML
 }
+
+
+
+
